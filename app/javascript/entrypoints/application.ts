7a02f13a4b03b52795308c6bc646c091994f22ea
Cameron Dutro <camertron@gmail.com>
2025-10-26 20:40:51 -0700

    Make entrypoint actually do something

--- app/javascript/entrypoints/application.ts
+++ app/javascript/entrypoints/application.ts
@@ -1,3 +1,7 @@
+import { Application } from "@hotwired/stimulus"
+
 import "../primer_view_components";
 import "../../components/file_browser";
 import "../../components/color_mode_toggle";
+
+Application.start();
